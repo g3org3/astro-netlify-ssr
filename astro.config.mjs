9b28ee677a20003ffaf4cdfefd9328c2dc8ae1e2
george <7jagjag@gmail.com>
2023-03-30 00:17:17 +0200

    add tailwind solid and netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), solidJs()],
+  output: "server",
+  adapter: netlify()
+});
